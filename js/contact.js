b275788aaee3df256d2cbd13bff4ae36cadf240d
FaisalJadallah <faisal.jadallah.fj@gmail.com>
2025-03-17 13:54:19 +0300

    alfiissssss

---
+++ js/contact.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // هنا يمكنك إضافة الكود اللازم لإرسال البيانات إلى الخادم
+    alert('تم إرسال الرسالة بنجاح');
+});
