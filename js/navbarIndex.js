9cc9254231d4d3699fdba5914600a6728d5e1e56
07ASN <HASAN.OMAR.20002@GMAIL.COM>
2025-03-19 01:50:51 +0300

    Modify CDN

--- js/navbarIndex.js
+++ js/navbarIndex.js
@@ -1,24 +1,9 @@
-
-tailwind.config = {
-    theme: {
-        extend: {
-            colors: {
-                primary: "#1E5631",
-                secondary: "#38755B",
-                light: "#F7F9F3",
-                accent: "#EAF0D7",
-                gold: "#D4AF37",
-                darkgold: "#B8860B",
-            },
-        },
-    },
-};
-
 fetch('html/navbar-index.html')
-    .then(response => response.text())
-    .then(data => {
+      .then(response => response.text())
+      .then(data => {
         document.getElementById('navbar-placeholder').innerHTML = data;
-});
+      })
+      .catch(error => console.error('Error fetching navbar:', error));
 
 fetch('html/footer.html')
       .then(response => response.text())
